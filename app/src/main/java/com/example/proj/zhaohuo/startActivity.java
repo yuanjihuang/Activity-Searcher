ebe9ab9c64d2ec2b39c090bae14dd1bfb667c9fb
14353429 <1036634669@qq.com>
2016-12-13 01:28:25 +0800

    增加APP启动画面，现在第一个启动的activity是startActivity

---
+++ app/src/main/java/com/example/proj/zhaohuo/startActivity.java
@@ -0,0 +1,39 @@
+package com.example.proj.zhaohuo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AnimationUtils;
+
+/**
+ * Created by Administrator on 2016/12/13.
+ */
+
+public class startActivity extends AppCompatActivity{
+    private final int SPLASH_DISPLAY_LENGHT = 4500; // 两秒后进入系统
+    private final int SPLASH_DISPLAY_LENGHT1 = 2000; // 两秒后进入系统
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        final CTextView cTextView = (CTextView) findViewById(R.id.cTextView);
+        final CTextView cTextView1 = (CTextView) findViewById(R.id.cTextView1);
+        cTextView.setText("找活", AnimationUtils.loadAnimation(this, R.anim.my_start_anim), 1500);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                cTextView1.setText("寻找属于你的活动", AnimationUtils.loadAnimation(startActivity.this, R.anim.my_start_anim), 200);
+            }
+        }, SPLASH_DISPLAY_LENGHT1);
+        //cTextView1.setText("寻找属于你的活动", AnimationUtils.loadAnimation(this, R.anim.my_start_anim), 400);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(startActivity.this, loginActivity.class);
+                startActivity(intent);
+                startActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
