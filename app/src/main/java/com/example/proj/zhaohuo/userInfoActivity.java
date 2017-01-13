ae8ae3811c5c8c5a54f82b696e82a514ccd032a2
XJBCoding <huangyuanji12@yeah.net>
2017-01-14 02:27:59 +0800

    用户信息连数据库

--- app/src/main/java/com/example/proj/zhaohuo/userInfoActivity.java
+++ app/src/main/java/com/example/proj/zhaohuo/userInfoActivity.java
@@ -1,19 +1,29 @@
 package com.example.proj.zhaohuo;
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import adapter.InfoAdapter;
 
@@ -23,12 +33,15 @@ public class userInfoActivity extends AppCompatActivity {
     private List<String> info;
     private List<String> index;
     private String get_user_info;
+    private String set_user_info;
     private RelativeLayout setUserInfo;
     private ListView user_info_listview;
     private TextView userIntro;
+    private int kind;
     private void initialize(){
         connectHelper = new ConnectHelper();
-        get_user_info = connectHelper.url+"/Service/get_user_info.jsp";
+        get_user_info = connectHelper.url+"Service/get_user_info.jsp";
+        set_user_info = connectHelper.url+"Service/set_user_info.jsp";
         user_info_listview = (ListView)findViewById(R.id.user_info_listview);
         userIntro = (TextView)findViewById(R.id.UserIntro);
         setUserInfo = (RelativeLayout)findViewById(R.id.setUserInfo);
@@ -43,9 +56,7 @@ public class userInfoActivity extends AppCompatActivity {
         info.add("12345677");
         info.add("1");
         info.add("大家好，我是来自移动信息工程的孙笔笔同学,我最喜欢凌晨四点的逸仙大道。");
-        userIntro.setText(info.get(4));
-        infoAdapter = new InfoAdapter(this,info,index);
-        user_info_listview.setAdapter(infoAdapter);
+        new DownloadWebpageText().execute(get_user_info+"?AcctName="+CurrentAcct.AcctName);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +75,8 @@ public class userInfoActivity extends AppCompatActivity {
                 edit_grade = (EditText)v.findViewById(R.id.edit_grade);
                 edit_id = (EditText)v.findViewById(R.id.edit_id);
                 edit_intro = (EditText)v.findViewById(R.id.edit_intro);
+                for(int i = 0; i < info.size(); i++)
+                    Log.d("info",info.get(i));
                 edit_school.setText(info.get(0));
                 edit_name.setText(info.get(1));
                 edit_id.setText(info.get(2));
@@ -80,7 +93,27 @@ public class userInfoActivity extends AppCompatActivity {
                         info.add(edit_name.getText().toString());
                         info.add(edit_id.getText().toString());
                         info.add(edit_grade.getText().toString());
+                        info.add(edit_intro.getText().toString());
                         userIntro.setText(edit_intro.getText().toString());
+                        String grade;
+                        switch (info.get(3)){
+                            default:
+                                grade = "freshman";
+                                break;
+                            case "1":
+                                grade = "freshman";
+                                break;
+                            case "2":
+                                grade = "sophomore";
+                                break;
+                            case "3":
+                                grade = "junior";
+                                break;
+                            case "4":
+                                grade = "senior";
+                                break;
+                        }
+                        new DownloadWebpageText().execute(set_user_info+"?AcctName="+CurrentAcct.AcctName+"&AcctKind="+kind+"&UserSchool="+info.get(0)+"&UserName="+info.get(1)+"&UserID="+info.get(2)+"&UserGrade="+grade+"&UserIntro="+info.get(4)+"&UserSex="+0);
                         infoAdapter.notifyDataSetChanged();
                     }
                 });
@@ -97,6 +130,53 @@ public class userInfoActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //new DownloadWebpageText().execute(get_user_info+"?Name="+CurrentAcct.AcctName);
+        new DownloadWebpageText().execute(get_user_info+"?AcctName="+CurrentAcct.AcctName);
+    }
+    private class DownloadWebpageText extends AsyncTask<String,Integer,List<String>> {
+        @Override
+        protected List<String> doInBackground(String... urls) {
+            try {
+                List<String> reList = connectHelper.downloadUrl(urls[0]);
+                return reList; //连接并下载数据
+            } catch (IOException e) {
+                e.printStackTrace();
+                List<String> reList = new LinkedList<>();//返回的字符串数组，若只有1个字符串取reList.get(0)
+                return reList;
+            }
+        }
+        @Override
+        protected void onPostExecute(List<String> result) {
+            if(result != null){
+                if(result.size() == 0){
+                    Toast.makeText(getApplicationContext(),"没有返回值，请再试一次！",Toast.LENGTH_SHORT).show();
+                }else{
+                    for(int i = 0; i < result.size(); i++)
+                        Log.d("relist",result.get(i));
+                    ///////解析json信息/////////////
+                    JSONArray ActList = JsonUtils.parse(result.get(0),"Act");//返回形式为多个键值对组成的序列
+                    JSONArray KindList = JsonUtils.parse(result.get(0),"User_Kind");
+                    try{
+                        JSONObject oj = KindList.getJSONObject(0);
+                        kind = oj.getInt("ActKind");
+                    }catch (Exception e){}
+                    info.clear();//清除当前信息
+                    try{
+                        JSONObject oj = ActList.getJSONObject(0);
+                        info.add(oj.getString("UserSchool"));//学校姓名学号年级
+                        info.add(oj.getString("UserName"));
+                        info.add(oj.getString("UserID"));
+                        info.add(oj.getString("UserGrade"));
+                        info.add(oj.getString("UserIntro"));
+                        userIntro.setText(info.get(4));
+                        infoAdapter = new InfoAdapter(userInfoActivity.this,info,index);
+                        user_info_listview.setAdapter(infoAdapter);
+                    }catch (Exception e){}
+                    //JSONArray FavoriteList = JsonUtils.parseAct(result.get(0));
+                    /*
+                    在这里更新UI
+                     */
+                }
+            }
+        }
     }
 }
